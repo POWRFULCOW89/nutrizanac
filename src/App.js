5c41d092c3927390970db59a1b5f84e710d681cd
Diego Melo <diego_14melo@hotmail.com>
2022-01-29 11:29:37 -0600

    removed firebase

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-
-import zapote from './assets/zapote.jpg'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Navigation from './components/Navigation';
-import Home from './components/Home';
-import Product from './components/Product';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import zapote from "./assets/zapote.jpg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-
   const isLandscape = () => {
     return window.innerHeight < window.innerWidth;
-  }
+  };
 
   return (
     <>
-      <div className='d-flex justify-content-center bg-success' style={styles.bg}>
-        <div className={`d-flex flex-column ${isLandscape() ? 'w-75' : ''} border rounded-bottom shadow p-3 mb-3 bg-white mx-0 px-0`} style={styles.main}>
+      <div
+        className="d-flex justify-content-center bg-success"
+        style={styles.bg}
+      >
+        <div
+          className={`d-flex flex-column ${
+            isLandscape() ? "w-75" : ""
+          } border rounded-bottom shadow p-3 mb-3 bg-white mx-0 px-0`}
+          style={styles.main}
+        >
           <Router className={`container-fluid`}>
-            <Navigation className='d-flex border'/>
+            <Navigation className="d-flex border" />
 
             <Switch>
-              <Route path='/' exact component={() => <Home />} />
-              <Route path='/producto' exact component={() => <Product />} />
-              <Route path='/contacto' exact component={() => <Contact />} />
+              <Route path="/" exact component={() => <Home />} />
+              <Route path="/producto" exact component={() => <Product />} />
+              <Route path="/contacto" exact component={() => <Contact />} />
             </Switch>
-            
+
             <Footer />
           </Router>
         </div>
       </div>
-      
     </>
   );
 }
@@ -46,13 +46,13 @@ const styles = {
     backgroundImage: `url(${zapote})`,
     // backgroundRepeat: 'repeat-y',
     // backgroundRepeat: 'repeat-y'
-    backgroundSize: 'cover'
+    backgroundSize: "cover",
     // object-fit: 'cover',
   },
   main: {
     // opacity: '0.95',
     // backgroundColor: 'rgba(245, 245, 245, 0.4)'
-  }
+  },
 };
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,71 +1,93 @@
-import React from 'react';
-import zapp from '../assets/Zapp1.jpg';
-import zapp2 from '../assets/Zapp2.jpg';
+import React from "react";
+import zapp2 from "../assets/Zapp2.jpg";
 
-const Section = props => {
-    return <>
-        <div className='col-md-11 mt-5'>
-            <div className={`display-4 border-bottom ${props.right ? 'text-right' : ''}`}>
-                {props.title}
-            </div>
+const Section = (props) => {
+  return (
+    <>
+      <div className="col-md-11 mt-5">
+        <div
+          className={`display-4 border-bottom ${
+            props.right ? "text-right" : ""
+          }`}
+        >
+          {props.title}
         </div>
-        <div className='col-md-11 mt-3'>
-            <div className={`${props.right ? 'text-right' : ''}`}>
-                {props.desc}
-            </div>
+      </div>
+      <div className="col-md-11 mt-3">
+        <div className={`${props.right ? "text-right" : ""}`}>{props.desc}</div>
+      </div>
+    </>
+  );
+};
+
+const Home = (props) => {
+  return (
+    <div className="container-fluid row d-flex justify-content-center mx-0 px-0 py-5">
+      <div className="col row d-flex justify-content-center align-items-center ">
+        <div
+          className="col  d-flex justify-content-center align-items-center flex-column"
+          // style={styles.zapote}
+
+          // style={{position: 'relative'}}
+        >
+          <div
+            // style={{position: 'absolute'}}
+            className="display-4 text-center my-2"
+          >
+            Donde la nutrición y la naturaleza se juntan.
+          </div>
+          <img alt="NutriZanac" src={zapp2} className="img-fluid" />
         </div>
-    </>;
-}
 
-const Home = props => {
+        <Section
+          title="¿Quiénes somos?"
+          desc="Somos estudiantes del Tecnológico de Veracruz, de la carrera de Ingeniería Mecatrónica"
+        />
 
-    return <div className='container-fluid row d-flex justify-content-center mx-0 px-0 py-5'>
-        <div className='col row d-flex justify-content-center align-items-center '>
-            <div className='col  d-flex justify-content-center align-items-center flex-column' 
-                // style={styles.zapote}
-                
-                // style={{position: 'relative'}}
-            >
-                <div 
-                // style={{position: 'absolute'}}
-                    className='display-4 text-center my-2'
-                >
-                    Donde la nutrición y la naturaleza se juntan.
-                </div>
-                <img alt='NutriZanac' src={zapp2} className='img-fluid'/>
+        <Section
+          right
+          title="¿Qué es?"
+          desc={
+            <div>
+              <b>NutriZanac</b> es un suplemento nutritivo artesanal, ideado
+              para personas que padecen diabetes y condiciones similares, sin
+              mermar su salud; aunque cualquiera, desde niños hasta personas de
+              avanzada edad, pueden consumirlo. Existen presentaciones en
+              barritas y muffins en diferentes tamaños.
             </div>
+          }
+        />
 
-            <Section title='¿Quiénes somos?' desc='Somos estudiantes del Tecnológico de Veracruz, de la carrera de Ingeniería Mecatrónica' />
-
-            <Section right title='¿Qué es?' desc={<div>
-                <b>NutriZanac</b> es un suplemento nutritivo artesanal, ideado para personas que padecen diabetes y condiciones similares, sin mermar su salud;
-                aunque cualquiera, desde niños hasta personas de avanzada edad, pueden consumirlo.
-                Existen presentaciones en barritas y muffins en diferentes tamaños.
-            </div>} /> 
-
-            <Section title='¿Qué contiene?' desc={<div>
-                El ingrediente principal de NutriZanac es el zapote negro, fruta mexicana de gran sabor y tradición popular. Además, NutriZanac está elaborado con:
-
-                <ul class="row mt-4 d-flex justify-content-center align-items-center px-0">
-                    <li class="list-group-item text-center m-1 border col-md-3 ">Nuez</li>
-                    <li class="list-group-item text-center m-1 border col-md-3 ">Avena</li>
-                    <li class="list-group-item text-center m-1 border col-md-3 ">Arándanos secos</li>
-                    <li class="list-group-item text-center m-1 border col-md-3 ">Aceite de coco</li>
-                    <li class="list-group-item text-center m-1 border col-md-3 ">Harina sin blanquear</li>
-                </ul>
-            </div>} /> 
-        </div>
+        <Section
+          title="¿Qué contiene?"
+          desc={
+            <div>
+              El ingrediente principal de NutriZanac es el zapote negro, fruta
+              mexicana de gran sabor y tradición popular. Además, NutriZanac
+              está elaborado con:
+              <ul class="row mt-4 d-flex justify-content-center align-items-center px-0">
+                <li class="list-group-item text-center m-1 border col-md-3 ">
+                  Nuez
+                </li>
+                <li class="list-group-item text-center m-1 border col-md-3 ">
+                  Avena
+                </li>
+                <li class="list-group-item text-center m-1 border col-md-3 ">
+                  Arándanos secos
+                </li>
+                <li class="list-group-item text-center m-1 border col-md-3 ">
+                  Aceite de coco
+                </li>
+                <li class="list-group-item text-center m-1 border col-md-3 ">
+                  Harina sin blanquear
+                </li>
+              </ul>
+            </div>
+          }
+        />
+      </div>
     </div>
-}
-
-const styles = {
-    zapote: {
-        height: 500,
-        // backgroundImage: `url("../assets/Zapp1.jpg")`,
-        backgroundImage: `url(${zapp})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover'
-    },
+  );
 };
 
 export default Home;
